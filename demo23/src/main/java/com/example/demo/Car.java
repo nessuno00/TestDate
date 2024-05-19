59f1f6821249bf44bcdff4b467fe301f81f1e999
nessuno00 <alect1@live.it>
2024-05-20 00:19:25 +0200

    D

---
+++ demo23/src/main/java/com/example/demo/Car.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+ @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+ @Column (nullable = false)
+      private String modelName;
+    @Column(nullable = false)
+
+    private String serialNumber;
+
+    private Double currentPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
